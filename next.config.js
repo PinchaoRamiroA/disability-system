7671f631738c56dbcf020115e9358f79a57f2ca7
PinchaoRamiroA <antonio.pinchao@utp.edu.co>
2026-05-14 19:23:22 -0500

    update a next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
 	reactStrictMode: false,
 	devIndicators: {
@@ -15,10 +20,4 @@ const nextConfig = {
 	},
 }
 
-module.exports = nextConfig
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer(nextConfig)
